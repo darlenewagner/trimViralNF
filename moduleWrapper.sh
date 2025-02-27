bb34c9582c0bd88c7a48a449a6f225001769065e
darlenewagner <ydn3@cdc.gov>
2025-02-27 11:05:50 -0500

    bash install script

--- moduleWrapper.sh
+++ moduleWrapper.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
- echo "export PATH=$PATH:/apps/x86_64/bio/nextflow/24.10.4/" > ~/.bash.d/nextflow.bash
+ echo "export PATH=$PATH:/apps/x86_64/bio/nextflow/24.10.4/:/apps/x86_64/BLAST+/2.13.0-gompi-2022a/bin/" >> ~/.bash.d/nextflow.bash
  echo "alias llA='ll nextflow'" >> ~/.bash.d/nextflow.bash
 
