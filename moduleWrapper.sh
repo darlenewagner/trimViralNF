7997d4ecd771c499490cb00dab04818260c8005d
darlenewagner <ydn3@cdc.gov>
2025-02-27 15:23:07 -0500

    bash get prereqs

--- moduleWrapper.sh
+++ moduleWrapper.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/bash
 
- echo "export PATH=$PATH:/apps/x86_64/bio/nextflow/24.10.4/:/apps/x86_64/BLAST+/2.13.0-gompi-2022a/bin/" >> ~/.bash.d/nextflow.bash
- echo "alias llA='ll nextflow'" >> ~/.bash.d/nextflow.bash
+## edit export PATH=$PATH:/path/to/my/nextflow/:/path/to/my/blast+/
 
+if [[ -d ~/.bash.d ]]; then
+    echo "export PATH=$PATH:/apps/x86_64/bio/nextflow/24.10.4/:/apps/x86_64/BLAST+/2.13.0-gompi-2022a/bin/" >> ~/.bash.d/nextflow.bash
+    echo "alias llA='ll nextflow'" >> ~/.bash.d/nextflow.bash
+else
+    mkdir ~/.bash.d
+    echo "export PATH=$PATH:/apps/x86_64/bio/nextflow/24.10.4/:/apps/x86_64/BLAST+/2.13.0-gompi-2022a/bin/" >> ~/.bash.d/nextflow.bash    
+    echo "alias llA='ll nextflow'" >> ~/.bash.d/nextflow.bash
+fi
