dc64cadb4ec534a01971e72ae5cf86b8823d0116
darlenewagner <ydn3@cdc.gov>
2025-02-27 16:14:36 -0500

    Comprehensive bash prereq. script

--- moduleWrapper.sh
+++ moduleWrapper.sh
@@ -3,10 +3,10 @@
 ## edit export PATH=$PATH:/path/to/my/nextflow/:/path/to/my/blast+/
 
 if [[ -d ~/.bash.d ]]; then
-    echo "export PATH=$PATH:/apps/x86_64/bio/nextflow/24.10.4/:/apps/x86_64/BLAST+/2.13.0-gompi-2022a/bin/" >> ~/.bash.d/nextflow.bash
+    echo "export PATH=/apps/x86_64/Perl/5.30.1-GCCcore-9.3.0-mt/bin/:/apps/x86_64/Java/17.0.6/bin/:$PATH:/apps/x86_64/bio/nextflow/24.10.4/:/apps/x86_64/BLAST+/2.13.0-gompi-2022a/bin/" >> ~/.bash.d/nextflow.bash
     echo "alias llA='ll nextflow'" >> ~/.bash.d/nextflow.bash
 else
     mkdir ~/.bash.d
-    echo "export PATH=$PATH:/apps/x86_64/bio/nextflow/24.10.4/:/apps/x86_64/BLAST+/2.13.0-gompi-2022a/bin/" >> ~/.bash.d/nextflow.bash    
+    echo "export PATH=/apps/x86_64/Perl/5.30.1-GCCcore-9.3.0-mt/bin/:/apps/x86_64/Java/17.0.6/bin/:$PATH:/apps/x86_64/bio/nextflow/24.10.4/:/apps/x86_64/BLAST+/2.13.0-gompi-2022a/bin/" >> ~/.bash.d/nextflow.bash
     echo "alias llA='ll nextflow'" >> ~/.bash.d/nextflow.bash
 fi
